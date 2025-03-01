5828cd65426f76962a921cc88a81dfb167e0c474
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2025-03-01 08:48:29 -0300

    Remove prisma migration from git ignore

---
+++ prisma/migrations/20250227211152_init_tasks/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "priority" "Priority" NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
